f84c3199cca9d164355afa9765fc66caae6dabc1
juan carlos macias <juancmaciassalvador@gmail.com>
2025-06-10 07:28:45 +0200

    Changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.jcms.juancarlosmacias"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -44,4 +44,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(libs.commonmark)
+    implementation(libs.appcompat.v161)
+
 }
--- app/src/main/java/com/jcms/juancarlosmacias/MainActivity.java
+++ app/src/main/java/com/jcms/juancarlosmacias/MainActivity.java
@@ -1,32 +1,53 @@
 package com.jcms.juancarlosmacias;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.widget.Button;
+import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
+    private Toolbar toolbar;
+    private WebView web;
     private String url = "http://www.juancarlosmacias.es/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Configurar Toolbar como ActionBar
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        web = findViewById(R.id.webView);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.loadUrl("file:///android_asset/principal.md");
 
     }
 
-    // Manejar el botón "Atrás" del dispositivo para navegar en la WebView
+
+
     @Override
-    public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.menuinicio){
+            web.loadUrl("file:///android_asset/principal.md");
+        }else if(item.getItemId() == R.id.menupolitica){
+            web.loadUrl("file:///android_asset/politica_privacidad.md");
+        }else{
+            return super.onOptionsItemSelected(item);
         }
+        return true;
     }
 }
